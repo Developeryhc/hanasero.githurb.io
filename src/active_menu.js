45287a5df6685c1dec7c60be4e4943a03fea64ac
DevHanasero <78771179+Developeryhc@users.noreply.github.com>
2024-12-27 10:44:12 +0900

    Observer-part3

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,7 +5,7 @@ let activeNavItem = navItems[0];
 
 const options = {
     rootMargin: "-20% 0px 0px 0px",
-    threshold: [0, 0.98]
+    threshold: [0.1, 0.98]
 };
 const visibleSections = sectionIds.map(() => false);
 
@@ -17,7 +17,7 @@ function observerCallback(entries) {
     entries.forEach(entry => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         visibleSections[index] = entry.isIntersecting;
-        selectLastOne = index === sectionIds.length -1 && entry.isIntersecting && entry.intersectionRatio >- 0.99;
+        selectLastOne = index === sectionIds.length -1 && entry.isIntersecting && entry.intersectionRatio >- 0.95;
     });
 
     const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
